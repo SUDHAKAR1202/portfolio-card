f0c5b7294798968e7a59d470016066ea15f59a75
Sudhakar <sudzcrazy94@gmail.com>
2026-03-23 09:14:32 +0530

    added toggle switch for dark/light mode

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,7 +1,7 @@
-function Avatar() {
+function Avatar({ image}) {
     return (
         <div className="avatar-container">
-            <img src="https://media.istockphoto.com/id/1542566108/photo/young-businessman-smiling-at-camera.jpg?s=2048x2048&w=is&k=20&c=fhjNCzVb8pijG09XiptTOKwucLpyD6jVy0VzsZW2FaY=" alt="Profile" className="avatar-image" />
+            <img src={image} alt="Profile" className="avatar-image" />
             <div className="avatar-border"></div>
         </div>
     )
--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 import Header from "./Header";
 import Avatar from "./Avatar";
 import PersonalInfo from "./PersonalInfo";
@@ -6,14 +7,53 @@ import Bio from "./Bio";
 import "./PortfolioCard.css";
 
 function PortfolioCard() {
+  const [isDark, setIsDark] = useState(false);
+  const [likes, setLikes] = useState(0);
+  const [avatarIndex, setAvatarIndex] = useState(0);
+
+  const avatars = [
+    "https://i.pravatar.cc/150?img=1",
+    "https://media.istockphoto.com/id/1542566108/photo/young-businessman-smiling-at-camera.jpg?s=2048x2048&w=is&k=20&c=fhjNCzVb8pijG09XiptTOKwucLpyD6jVy0VzsZW2FaY=",
+    "https://i.pravatar.cc/150?img=3",
+  ];
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  }
+
+  const handleLike = () => {
+    setLikes(likes + 1);
+  };
+
+  const changeAvatar = () => {
+    setAvatarIndex((prev)=> (prev + 1) % avatars.length);
+  }
+
+  const showAlert = () => {
+    alert("Hello! Thanks for visiting profile");
+  }
   return (
-    <div className="portfolio-card">
+    <div className={`portfolio-card ${isDark ? "dark" : "light"}`}>
       <Header />
 
       <div className="card-content">
-        <Avatar />
+        <Avatar  image={avatars[avatarIndex]}/>
         <PersonalInfo />
         <Bio />
+
+        <div className="actions">
+          <button onClick={toggleTheme}>
+            {isDark ? "Light Mode": "Dark Mode"}
+
+          </button>
+
+          <button onClick={changeAvatar}>Change Photo</button>
+
+          <button onClick={showAlert}>Alert</button>
+
+          <button onClick={handleLike}>Like ({likes})</button>
+
+        </div>
       </div>
     </div>
   );
